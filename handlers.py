Khan/appengine-mapreduce

Add compression to JSON-serialized input readers
Summary:
This compresses the JSON-serialized input readers with zlib.

A follow-up to: http://phabricator.khanacademy.org/D6147

Per @tom's comments, I'm going to leave `json_util.py` as is to minimize changes, since removing spaces in the `,` and `:` separators doesn't give us much. Simple enough!

Test Plan: Run locally and on znd

Reviewers: tom, mattfaus

Reviewed By: mattfaus

CC: jace, eliana, tom

Differential Revision: http://phabricator.khanacademy.org/D6215

trunk/python/src/mapreduce/handlers.py
@@ -30,6 +30,7 @@ import random
 import sys
 import time
 import traceback
+import zlib
 from mapreduce.lib import simplejson
 
 from google.appengine.ext import ndb
@@ -1235,7 +1236,8 @@ class KickOffJobHandler(base_handler.TaskQueueHandler):
           state.mapreduce_spec.mapper)
     else:
       readers = [input_reader_class.from_json_str(json) for json in
-                 simplejson.loads(serialized_input_readers.payload)]
+                 simplejson.loads(zlib.decompress(
+                 serialized_input_readers.payload))]
 
     if not readers:
       return None, None
@@ -1250,7 +1252,8 @@ class KickOffJobHandler(base_handler.TaskQueueHandler):
       serialized_input_readers = model._HugeTaskPayload(
           key_name=serialized_input_readers_key, parent=state)
       readers_json_str = [i.to_json_str() for i in readers]
-      serialized_input_readers.payload = simplejson.dumps(readers_json_str)
+      serialized_input_readers.payload = zlib.compress(simplejson.dumps(
+                                                       readers_json_str))
     return readers, serialized_input_readers
 
   def _setup_output_writer(self, state):

scr-fisdom/mapreduce

handlers.py
@@ -29,6 +29,7 @@ import random
 import sys
 import time
 import traceback
+import zlib
 
 try:
   import json
@@ -1442,7 +1443,8 @@ class KickOffJobHandler(base_handler.TaskQueueHandler):
       readers = input_reader_class.split_input(split_param)
     else:
       readers = [input_reader_class.from_json_str(_json) for _json in
-                 json.loads(serialized_input_readers.payload)]
+                 json.loads(zlib.decompress(
+                 serialized_input_readers.payload))]
 
     if not readers:
       return None, None
@@ -1457,7 +1459,8 @@ class KickOffJobHandler(base_handler.TaskQueueHandler):
       serialized_input_readers = model._HugeTaskPayload(
           key_name=serialized_input_readers_key, parent=state)
       readers_json_str = [i.to_json_str() for i in readers]
-      serialized_input_readers.payload = json.dumps(readers_json_str)
+      serialized_input_readers.payload = zlib.compress(json.dumps(
+                                                       readers_json_str))
     return readers, serialized_input_readers
 
   def _setup_output_writer(self, state):